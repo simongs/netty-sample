8bd19f76e4be44159878475c8bdd8ad88a2a45a3
SIMONGS\NHNEnt <simhongsup@nhnent.com>
2016-11-08 16:55:50 +0900

    프로퍼티 테스트 커밋

---
+++ src/test/java/com/dasoulte/simons/server/core/DaemonPropertiesTest.java
@@ -0,0 +1,17 @@
+package com.dasoulte.simons.server.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by NHNEnt on 2016-11-08.
+ */
+public class DaemonPropertiesTest {
+
+    @Test
+    public void test() {
+        System.out.println(DaemonProperties.PAYCO_API_HOST);
+    }
+
+}
